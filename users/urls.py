8b4b0fc228b262ed167a970b98a24a7d4419e885
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-11-14 15:37:20 +0100

    Added logout and change password

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView,LogoutView, PasswordChangeView
 from django.urls import path
 
 # Create your views here
@@ -8,5 +8,7 @@ urlpatterns = [
     path('dashboard/',views.dashboardView,name="dashboard"),
     path('login/',LoginView.as_view(),name="login"),
     path('register/',views.registerView,name="register"),
+    path('change_password/', views.MyPasswordChangeView.as_view(), name="change_password"),
+    path('change_password/done/', views.MyPasswordResetDoneView.as_view(), name='password_change_done'),
     path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
 ]
