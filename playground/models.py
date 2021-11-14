ec536c99c70b034a5cdcc8e476cb77abd4d9f34f
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-11-14 13:32:00 +0100

    Added circuit model

--- playground/models.py
+++ playground/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Circuit (models.Model):
-    name = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=30)
     land = models.CharField(max_length=30)
 
-
-spa = Circuit.objects.create(name= "Spa-Francorchamps", land= "Belgium")
+# adds a circuitobject
+# spa = Circuit.objects.create(name= "Spa-Francorchamps", land= "Belgium")
