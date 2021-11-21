53b1990f99eb8bc7e640bf6a9eec39cbd2ed5fd8
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-11-21 15:38:28 +0100

    Added api app on django

--- lappygp/settings.py
+++ lappygp/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'corsheaders'
+    'rest_framework',
+    'corsheaders',
     'playground',
-    'users'
+    'users',
+    'api',
 ]
 
 MIDDLEWARE = [
