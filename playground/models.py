a979745331f6081a55212dd43723ff46f6803fad
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-11-21 17:43:44 +0100

    Added length to circuit database

--- playground/models.py
+++ playground/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Circuit (models.Model):
     name = models.CharField(max_length=30)
     land = models.CharField(max_length=30)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    length = models.fields.IntegerField()
+
+# class Lap(models.Model):
+
 
 # adds a circuitobject
 # spa = Circuit.objects.create(name= "Spa-Francorchamps", land= "Belgium")
