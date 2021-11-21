07e7a1e4cacbbb875e269ac2519a1cfe857df19d
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-11-21 16:23:48 +0100

    Added tokens to every user

--- lappygp/urls.py
+++ lappygp/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 # playground/hello
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('auth/', obtain_auth_token),
     path('playground/', include('playground.urls')),
     path('users/', include('users.urls') )
     
