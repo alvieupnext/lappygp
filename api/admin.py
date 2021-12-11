8fbd0004176a91e32ec3940664011eb4e8fc0677
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-12-11 18:01:41 +0100

    Comments for API: admin.py

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Circuit, Lap, UserFollowing, UserProfile
 
 # Register your models here.
-
+# This is used for development
 admin.site.register(Circuit)
 admin.site.register(Lap)
 admin.site.register(UserFollowing)
